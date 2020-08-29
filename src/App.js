9dd9e9c3bd1654ddbab5ad9ab3c29cf29a9610f7
assiri <assiri@hotmail.com>
2020-08-29 03:48:12 +0300

    button added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-
 function App() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-  const goToPrevPage = () =>
-  setPageNumber(pageNumber-1)
-const goToNextPage = () =>
-setPageNumber(pageNumber+1)
+const onDocumentLoadSuccess=({ numPages }) =>setNumPages(numPages)
+const goToPrevPage = () =>setPageNumber(pageNumber-1)
+const goToNextPage = () => setPageNumber(pageNumber+1)
 
   return (
     <div>
-       <nav>
+       <div style={{display: 'flex'}}>
           <button onClick={goToPrevPage}>Prev</button>
+          <p>{pageNumber} / {numPages}</p>
           <button onClick={goToNextPage}>Next</button>
-        </nav>
-      <Document
-        file="1.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
+        </div>
+      <Document file="1.pdf" onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>Page {pageNumber} of {numPages}</p>
+      
     </div>
   );
 }
